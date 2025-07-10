37c0d2a7834a4682bb3f62f6471140ba6df39c10
Tomaz Vieira <tom.fogaca@gmail.com>
2025-07-10 11:31:30 +0200

    Cleanup tests a bit

--- aspartial/tests/test_derive.rs
+++ aspartial/tests/test_derive.rs
@@ -1,36 +1,23 @@
 use ::aspartial::AsPartial;
 #[derive(AsPartial, PartialEq, Eq, Debug, serde::Serialize)]
 struct SomeStruct {
-    #[serde(default = "default_number")]
-    a: u32,
-    b: String,
+    normal_string_field: String,
+    #[serde(default = "seven")]
+    defaults_to_7: u32,
+    #[serde(default)]
+    defaults_to_default: bool,
 }
 
-fn default_number() -> u32 {
+fn seven() -> u32 {
     7
 }
 
 #[test]
 fn test_derive_simple(){
 
-    let raw = serde_json::json!(
-        { "a": 123, "b": "lala" }
-    );
+    let raw = serde_json::json!( {} );
     let parsed:  PartialSomeStruct = serde_json::from_value(raw).unwrap();
-    assert_eq!(parsed.a, 123);
-    assert_eq!(parsed.b, Some("lala".into()));
-
-    let raw = serde_json::json!(
-        { "a": 123 }
-    );
-    let parsed:  PartialSomeStruct = serde_json::from_value(raw).unwrap();
-    assert_eq!(parsed.a, 123);
-    assert_eq!(parsed.b, None);
-
-    let raw = serde_json::json!(
-        { "b": "lala"}
-    );
-    let parsed:  PartialSomeStruct = serde_json::from_value(raw).unwrap();
-    assert_eq!(parsed.a, 7);
-    assert_eq!(parsed.b, Some("lala".into()));
+    assert_eq!(parsed.normal_string_field, None);
+    assert_eq!(parsed.defaults_to_7, 7);
+    assert_eq!(parsed.defaults_to_default, <bool as Default>::default());
 }
