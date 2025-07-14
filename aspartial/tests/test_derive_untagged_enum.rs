727c9d22916e30b1eb99a27f148660d98d0ea69f
Tomaz Vieira <tom.fogaca@gmail.com>
2025-07-14 19:31:34 +0000

    Adds test for untagged enums

---
+++ aspartial/tests/test_derive_untagged_enum.rs
@@ -0,0 +1,37 @@
+
+use ::aspartial::AsPartial;
+
+#[derive(AsPartial)]
+#[aspartial(name = PartialSomeStruct)]
+#[aspartial(attrs( #[derive(PartialEq, Eq, Debug, ::serde::Deserialize)] ))]
+#[allow(dead_code)]
+struct SomeStruct {
+    a: u32,
+    b: String,
+}
+
+#[derive(AsPartial)]
+#[aspartial(name = PartialSomeEnum)]
+#[aspartial(attrs( #[derive(::serde::Deserialize)] ))]
+#[aspartial(attrs( #[serde(try_from="::serde_json::Value")] ))]
+#[aspartial(attrs( #[serde(tag = "untagged")] ))]
+#[aspartial(derive(TryFrom<::serde_json::Value>))]
+#[allow(dead_code)]
+enum SomeEnum {
+    StringVariant(String),
+    StructVariant(SomeStruct)
+}
+
+#[test]
+fn test_derive_enum(){
+    let raw = serde_json::json!(
+        {
+            "a": 1234,
+            "b": "some string",
+        }
+    );
+    let parsed: PartialSomeEnum = serde_json::from_value(raw).unwrap();
+    let expected_struct_variant = PartialSomeStruct{a: Some(1234), b: Some("some string".to_owned())};
+    assert_eq!(parsed.struct_variant, Some(expected_struct_variant));
+    assert_eq!(parsed.string_variant, None);
+}
