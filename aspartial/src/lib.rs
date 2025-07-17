5edf9fbc38cca181a1803f78c0cf1fae46cf6d36
Tomaz Vieira <tom.fogaca@gmail.com>
2025-07-17 16:42:51 +0200

    Move README.md to project root

--- aspartial/src/lib.rs
+++ aspartial/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../README.md"))]
 
 /// A type that can have a "partial" or "incomplete" representation. These are
 /// usually serializable types, and their "partial" representations are objects
